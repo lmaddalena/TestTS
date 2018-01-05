69f47cfc02fda7f4a4d10f5cab97a6ab712300d3
l.maddalena <l.maddalena@almaviva.it>
2018-01-05 14:48:00 +0100

    .

--- dist/main.js
+++ dist/main.js
@@ -2,10 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var lodash = require("lodash");
 var Persons = require("./student");
+var zlib = require("zlib");
+var fs = require("fs");
 console.log("Test Typescript");
 console.log("===============");
 var s = lodash.padStart("Hello World!!!", 20, " ");
 console.log(s);
 var p = new Persons.Student("John", "Smith");
 console.log(p.GetFullName());
+var gzip = zlib.createGzip();
+var inp = fs.createReadStream('input.txt');
+var out = fs.createWriteStream('input.txt.gz');
+inp.pipe(gzip).pipe(out);
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import * as lodash from  "lodash";
 import * as Persons from "./student";
+import * as zlib from "zlib";
+import * as fs from "fs";
 
 console.log("Test Typescript");
 console.log("===============");
@@ -9,3 +11,10 @@ console.log(s);
 
 let p = new Persons.Student("John", "Smith");
 console.log(p.GetFullName());
+
+let gzip = zlib.createGzip();
+let inp = fs.createReadStream('input.txt');
+let out = fs.createWriteStream('input.txt.gz');
+
+inp.pipe(gzip).pipe(out);
+
